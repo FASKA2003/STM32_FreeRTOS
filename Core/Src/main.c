8221e68e70d9c0a7363ab3453cd4981a7d63cf6d
= <=>
2023-11-18 19:57:01 +0100

    Creating two tasks :1. Reset the System core clock

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,14 +1,13 @@
-#include "stm32f446xx.h"
 #include <main.h>
-#include <stm32f4xx_hal.h>
-#include <stm32f4xx_hal_gpio.h>
+#include <stm32f4xx.h>
+#include <stm32f4xx_hal_rcc.h>
 
-int main(int argc, char *argv[]) {
-  GPIO_InitTypeDef *GPIO_Init;
-  GPIO_Init->Pin = GPIO_PIN_5;
-  GPIO_Init->Mode = OUTPUT_PP;
+int main(void) {
+  // reset the RCC clock configuration to the default state :
+  HAL_RCC_DeInit();
 
-  HAL_GPIO_Init(GPIOA, GPIO_Init);
-  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+  // Update the SystemCoreClock variable
+  SystemCoreClockUpdate();
+
   return 0;
 }
